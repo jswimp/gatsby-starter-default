0c67004946cf2b14b396e3d18b38bdc6a8352605
jswimp <40179766+jswimp@users.noreply.github.com>
2018-06-21 20:03:30 -0700

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -5,6 +5,7 @@ const SecondPage = () => (
   <div>
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/260MLsqtWVA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
     <Link to="/">Go back to the homepage</Link>
   </div>
 )

bb068bdf5ba29a5081a7eb5e9493ac1e6c5d54df
jswimp <40179766+jswimp@users.noreply.github.com>
2018-06-21 20:25:44 -0700

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -8,18 +8,7 @@ const SecondPage = () => (
   <iframe width="560" height="315" src="https://www.youtube.com/embed/260MLsqtWVA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
     
   <p><Link to="/">Go back to the homepage</Link></p>
- 
-  <form name="new music fridays - subscribe" womp>
-  <p>
-    <label>Name <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Email <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
+
   
 )
 
